c78db0d0992b60d7e55bd2d5a6a131536ff528ad
Chwakillah <syakillahnachwa081@gmail.com>
2024-01-18 20:30:55 +0700

    menambahkan layout dan mengedit index.php

---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Daftar Akun</h3>
+    <form action="">
+        <input type="text" name="" placeholder="username"name="username">
+        <input type="text" name="" placeholder="password"name="password">
+        <button type="submit">Daftar Sekarang</button>
+    </form>
+</body>
+</html>
